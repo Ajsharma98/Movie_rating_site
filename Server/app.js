067ad43933ca172ee11643a26824e8e958dbe8f9
Anuj Sharma <anujsharmaiitbombay2024@gmail.com>
2024-10-14 12:51:09 +0530

    code for app.js

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const movieRoutes = require('./routes/movieRoutes');// importing the routes file for book
+const userRoutes = require('./routes/loginRoutes');
 const app = express();
+
+app.use(express.json());
+app.use('/movies', movieRoutes);
+app.use('/users',userRoutes);
 const PORT = 4000;
 app.listen(PORT, () => {        // starting the express server on given port
     console.log(`Server is running on port ${PORT}`);
