336b92e1e7420661414a244d0bd6bc6d67f7df42
Anuj Sharma <anujsharmaiitbombay2024@gmail.com>
2024-10-14 12:27:50 +0530

    added login routes

---
+++ Server/routes/loginRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();// creating the instance of route 
+const loginController = require('../controllers/loginController');// importing the loginController
+// Route for user login
+router.post('/login', loginController.loginUsers);// post route for the login and implementing loginUser function 
+
+// Route for user signup
+router.post('/signup', loginController.signupUsers);// post route for the signup and implementing the signup function
+
+// Route for user logout
+router.post('/logout',  loginController.logoutUsers);// post route for the logout and implementing the logout function
+
+module.exports = router;// exporting the router instance for usage in app.js
