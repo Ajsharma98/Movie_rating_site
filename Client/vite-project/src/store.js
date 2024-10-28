a3c39621e4ee832a27b0c746e5467e5c9410d4c9
Anuj Sharma <anujsharmaiitbombay2024@gmail.com>
2024-10-28 11:48:29 +0530

    fetchall rating working code

--- Client/vite-project/src/store.js
+++ Client/vite-project/src/store.js
@@ -6,7 +6,7 @@ export const totalPages = writable(1);
 export const displayedData = writable([]);
 // export const page_num=writable('');
 
-export const fetchMovies = async () => {
+export const fetchAllMovies = async () => {
   try {
     const page_num = get(page);
     const limit_val = get(limit);
@@ -43,7 +43,7 @@ export const fetchMovies = async () => {
   }
 };
 
-export const fetchUsers = async () => {
+export const fetchAllUsers = async () => {
   try {
     const page_num = get(page);
     const limit_val = get(limit);
@@ -65,6 +65,41 @@ export const fetchUsers = async () => {
       const data = await response.json();
       console.log("Fetched Data:", data);
       displayedData.set(data.Users);
+      totalPages.set(data.totalPages);
+    } else {
+      console.error(
+        "Failed to fetch data:",
+        response.status,
+        response.statusText
+      );
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+};
+
+export const fetchAllRatings = async () => {
+  try {
+    const page_num = get(page);
+    const limit_val = get(limit);
+    const token = localStorage.getItem("jwttoken");
+    if (!token) {
+      console.error("No token found, unauthorized accesss");
+      return;
+    }
+    const response = await fetch(
+      `http://localhost:4000/rating?page=${page_num}&limit=${limit_val}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (response.ok) {
+      const data = await response.json();
+      console.log("Fetched Data:", data);
+      displayedData.set(data.Ratings);
       totalPages.set(data.totalPages);
     } else {
       console.error(
