54b09557f8d7e3570090541a4e64d5b846b4f81b
Anuj Sharma <anujsharmaiitbombay2024@gmail.com>
2024-11-25 11:35:15 +0530

    add rating button position and shape modification

--- Client/vite-project/src/lib/Components/Ratings/AddRating.svelte
+++ Client/vite-project/src/lib/Components/Ratings/AddRating.svelte
@@ -16,7 +16,7 @@
   }
 </script>
 
-<button class="button" on:click={openModal}>Add Rating</button>
+<button class="button" on:click={openModal}>+</button>
 
 {#if showModal}
   <div class="modal-overlay">
@@ -27,19 +27,35 @@
 <style>
   .button {
     padding: 10px 20px;
-    background-color: #28a745;
+    background-color: rgb(65, 74, 76);
     color: white;
     border: none;
     cursor: pointer;
-    border-radius: 5px;
-    /* position: absolute; */
+    /* border-radius: 5px; */
+    position: absolute;
+    top: 2px;
+    left: 0px;
+    height: 50px;
+    font-size: 20px;
+    clip-path: polygon(
+      50% 0%,
+      61% 35%,
+      98% 35%,
+      68% 57%,
+      79% 91%,
+      50% 70%,
+      21% 91%,
+      32% 57%,
+      2% 35%,
+      39% 35%
+    );
     /* bottom: 10px;
     left: 50%;
     transform: translateX(-50%); */
   }
 
   .button:hover {
-    background-color: #218838;
+    background-color: rgb(59, 68, 75);
   }
 
   .modal-overlay {
